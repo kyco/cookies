55be5404ce86ded5dbd6ae18de940cc5cd359e0e
Cornelius <cornelius@kyco.co.za>
2013-10-12 22:14:34 +0200

    Added update function and optimised code.

--- kyco.cookies.js
+++ kyco.cookies.js
@@ -1,52 +1,96 @@
-// kyco.cookies brought to you by www.kyco.co.za. Copyright 2013 Cornelius Weidmann. Distributed under the GPL.
-// Original idea from http://www.quirksmode.org/js/cookies.html.
-// This code does not require jQuery, simply copy and paste to use the functions.
-function createCookie(name, value, days) {
-	// Creates a cookie with the given name and value. The days attribute is optional.
-	// Not setting the days attribute will create a cookie that expires at the end of the session.
-	// Setting the days attribute to 0 will create a cookie that expires at the end of the session.
-	// Setting the days attribute to -1 will create a cookie that expires immediately, i.e. deletes the cookie.
+/*********************************************\
 
-	// Usage, e.g.: createCookie('newcookie', 'testing', 2);
-	// Cookie will hold the following string: newcookie=testing; expires=<2 days from creation>;
+	kyco.cookies
+	v1.1.0
 
-	// Manually create a cookie string, e.g.: document.cookie = newcookie=true; expires=Thu May 10 2012 08:27:20 UTC; path=/;
-	// Note: appending the UTC in the expires string will automatically add or subtract the hours for the given time zone.
+	Brought to you by http://www.kyco.co.za
+	Copyright 2013 Cornelius Weidmann
 
-	if (days) {
-		// Set the expiry date to expire after given amount of days.
-		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		var expires = '; expires=' + date.toUTCString() + ';';
-	} else {
-		var expires = ''; // Set expiry date to expire upon session close.
+	Distributed under the GPL
+
+	Original idea from:
+	http://www.quirksmode.org/js/cookies.html
+
+	This code does not require jQuery,
+	simply copy and paste to use functions.
+
+\*********************************************/
+
+function createCookie(name, value, days, path) {
+	// Creates a cookie with the given name and value.
+	// The days and path parameters are optional.
+
+	/*	
+	**	Not setting the days parameter or passing in zero will 
+	**	create a cookie which expires at the end of the session.
+	**	Setting the days parameter to -1 will create a cookie
+	**	which expires immediately, i.e. deletes the cookie.
+	**
+	**	Manually creating a cookie string looks something like this, e.g.
+	**
+	**	document.cookie = foo=bar; expires=Thu May 10 2012 08:27:20 UTC; path=/;
+	**
+	**	Note: appending the "UTC" in the expires string will automatically
+	**	add or subtract the hours for the given time zone.
+	*/
+
+	if (name === undefined) {
+		return;
 	}
 
-	document.cookie = name + '=' + value + expires + 'path=/'; // Creates the cookie.
+	days = days || 0;
+	path = path || '/';
+
+	if (days != 0) {
+		// Set expiry date to UTC Date/Time string for days parameter
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		days = date.toUTCString();
+	} else {
+		// Set expiry date to expire upon session close
+		days = '';
+	}
+
+	// Create the cookie
+	document.cookie = name + '=' + value + '; expires=' + days + '; path=' + path + ';';
 }
 
 function readCookie(name) {
 	// Searches for the given name under cookies of current domain.
 	// Returns the value of the cookie if found, otherwise, returns null.
 
-	var nameWithEquals = name + '=';
-	var cookieArray = document.cookie.split(';');
-	var cookieArrayLength = cookieArray.length;
+	if (name === undefined) {
+		return;
+	}
 
-	for (var i = 0; i < cookieArrayLength; i++) {
-		var cookie = cookieArray[i];
+	var cookies = document.cookie.split(';');
+
+	for (var i = 0, n = cookies.length; i < n; i++) {
+		var cookie = cookies[i];
+		var len = cookie.length;
+
 		while (cookie.charAt(0) === ' ') {
-			cookie = cookie.substring(1, cookie.length);
+			cookie = cookie.substring(1, len);
 		}
-		if (cookie.indexOf(nameWithEquals) === 0) {
-			return cookie.substring(nameWithEquals.length, cookie.length);
+
+		if (cookie.indexOf(name) === 0) {
+			return cookie.substring(name.length + 1, len);
 		}
 	}
 
 	return null;
 }
 
-function deleteCookie(name) {
-	// Creates a cookie with expiry set to -1, which will delete the cookie immediately.
-	createCookie(name, '', -1);
+function updateCookie(name, value, days, path) {
+	// Updates a cookie with new value and optional
+	// days and path parameter.
+
+	createCookie(name, value, days, path);
+}
+
+function deleteCookie(name, path) {
+	// Creates a cookie with expiry set to -1,
+	// i.e. which deletes the cookie immediately.
+
+	createCookie(name, '', -1, path);
 }
